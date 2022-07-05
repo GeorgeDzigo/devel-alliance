49fa7319ddc1542bf46df36c2a351e12fc3cac7c
Giorgi Dzigoshvili <georgedzigo@gmail.com>
2022-07-05 10:04:44 +0300

    Made dynamic

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Developers aliance</title>
-    <link rel="stylesheet" href="styles/css/style.css">
-</head>
-<body>
+<?php
+    $root = $_SERVER['DOCUMENT_ROOT'] . '/delliance';
+
+    require("$root/public/parts/top.php");
+?>
     
-</body>
-</html>
+
+<?php
+    require("$root/public/parts/bottom.php");
+?>
